5f21c978ade0bf501c372310dcafb134b64d346f
Paul Chua <paul.chua96@gmail.com>
2024-11-03 13:51:10 -0500

    Project upload

---
+++ spotfiy_bot.py
@@ -0,0 +1,73 @@
+import os
+import discord
+from discord import app_commands
+
+from spotifyapi import *
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = discord.Client(intents = discord.Intents.default())
+tree = app_commands.CommandTree(client)
+discord_id = os.getenv('discord_id')
+channelid = os.getenv('channelid')
+
+
+@client.event
+async def on_ready():
+    channelid = os.getenv('channelid')
+    
+    await tree.sync()
+    await client.get_channel(int(channelid)).send("Hello, bot is online now")
+    
+    print(f'{client.user} is now online!')
+
+
+#guild=discord.Object(id=discord_id)
+
+@tree.command(name = "current", description= "Displays current song")
+async def current_command(phrase: discord.Interaction):
+    await phrase.response.send_message((get_current_playing()))
+
+@tree.command(name = "skip", description = "Skips current song")
+async def skip_command(phrase:discord.Interaction):
+    await phrase.response.send_message(skip_song())
+    
+@tree.command(name = "previous", description = "Skips current song")
+async def skip_command(phrase:discord.Interaction):
+    await phrase.response.send_message(prev_song())
+
+@tree.command(name = "volume", description= "Set volume")
+@app_commands.describe(target_volume = "The volume to set Spotify to")
+async def volume_command(phrase: discord.Interaction, target_volume: int):
+    await phrase.response.send_message(f"Volume set to {set_volume(target_volume)}")
+
+
+@tree.command(name = "pause", description = "Pause")
+async def pause_command(phrase: discord.Interaction):
+    await phrase.response.send_message(control(True))
+
+@tree.command(name = "play", description= "Resume")
+async def play_command(phrase: discord.Interaction):
+    await phrase.response.send_message(control(False))
+
+@tree.command(name = "seek",  description = "skips x amount of seconds")
+@app_commands.describe(seconds=  "Amount of seconds wanted to skip")
+async def skip_command(phrase: discord.Interaction, seconds: int):
+    await phrase.response.send_message(f"Skipped {skip(seconds)} seconds")
+
+#For toggle have it take what they want (repeat, context, shuffle)
+@tree.command(name = "toggle", description= "Toggle between repeating or shuffling your playlist")
+@app_commands.describe(mode = "The mode to play the playlist")
+async def toggle_command(phrase: discord.Interaction, mode: str):
+    await phrase.response.send_message(toggle_mode(mode))
+
+    
+@tree.command(name = "queue", description = "View your queue")
+async def queue_command(phrase:discord.Interaction):
+    await phrase.response.send_message(view_queue())
+
+    
+    
+    
+client.run(os.getenv('token'))
---
+++ spotifyapi.py
@@ -0,0 +1,102 @@
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+from dotenv import load_dotenv
+
+
+
+load_dotenv()
+
+client_id = os.getenv("client_id")
+client_secret = os.getenv("client_secret")
+redirect_uri = "http://localhost:3000"
+
+#For multiple scopes just add them together one word
+spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id, client_secret= client_secret, 
+                                                                              redirect_uri= redirect_uri, scope = "user-read-currently-playing user-modify-playback-state user-read-playback-state"))
+
+
+def get_current_playing():
+    artists = ""
+    result =  spotify.current_user_playing_track()
+    for s in result['item']['artists']:
+        artists += s['name'] + ", "
+    return result['item']['name'] + " by " + artists.rstrip(', ')
+
+def skip_song():
+    spotify.next_track()
+    return "Song skipped."
+
+def prev_song():
+    spotify.previous_track()
+    return "Previous track playing"
+    
+
+
+def set_volume(volume):
+    try:
+        if(volume > 100):
+            volume = 100
+        elif(volume < 0):
+            volume = 0 
+        spotify.volume(volume_percent=volume)
+        return volume
+    except TypeError:
+        return "Volume must be a number"
+
+def control(stop):
+    try:
+        if stop == True:
+            spotify.pause_playback()
+            return "Paused"
+        else:
+            spotify.start_playback()
+            return "Resuming"
+    except TypeError:
+        return "Must be true or false"
+
+def view_queue():
+    #I hate spelling this word
+    q = spotify.queue()
+    song_display = ""
+    
+    
+    for item in q['queue']:
+        s = item['artists']
+        song_display += f"{item['name']} by {s[0]['name']}\n"
+    return song_display
+            
+
+
+def skip(seconds):
+    try:
+        seconds *= 1000
+        ## if the number of seconds is bigger than song length it skips
+        spotify.seek_track(seconds)
+        return seconds / 1000
+    except:
+        return "Value must be a positive number"
+
+def toggle_mode(mode):
+    match mode.lower():
+        case "track":
+            spotify.repeat("track")
+            return "Track set on repeat"
+        #Context - the repeat without 1
+        case "context":
+            spotify.repeat("context")
+            return "Playlist set on repeat"
+        case "shuffle": 
+            spotify.shuffle(True)
+            return "Playlist on shuffle"
+        case _:
+            return "Invalid command, please use 'Track, shuffle, or context' "
+
+
+
+            
+    
+
+
+
